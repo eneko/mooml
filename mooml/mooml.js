fbd5fb69b5633770a019c504560100357ba3218b
Eneko Alonso <eneko.alonso@gmail.com>
2010-01-14 14:32:39 -0800

    Initial commit

---
+++ mooml/mooml.js
@@ -0,0 +1,69 @@
+﻿/**
+* @class MooML
+* @author Eneko Alonso (http://enekoalonso.com)
+* Based on Ed Spencer's Jaml (http://edspencer.github.com/jaml)
+* MooML is a Mootools based version of Jaml which makes HTML generation easy and pleasurable.
+*/
+
+MooML = new (new Class({
+	templates: {},
+	engine: {
+		nodes: []
+	},
+	tags: [
+		"html", "head", "body", "script", "meta", "title", "link", "script",
+		"div", "p", "span", "a", "img", "br", "hr",
+		"table", "tr", "th", "td", "thead", "tbody",
+		"ul", "ol", "li",
+		"dl", "dt", "dd",
+		"h1", "h2", "h3", "h4", "h5", "h6", "h7",
+		"form", "input", "label"
+	],
+
+	initialize: function() {
+		this.generateTags();
+	},
+
+	register: function(name, code) {
+		this.templates[name] = function(data) {
+			return this.evaluate(code, data);
+		} .bind(this);
+	},
+
+	render: function(name, data) {
+		return this.templates[name](data);
+	},
+
+	evaluate: function(code, data) {
+		with (this.engine) eval('(' + code + ')(data)');
+		//console.log('this.engine.nodes: ', this.engine.nodes);
+		var elements = new Elements(this.engine.nodes.filter(function(node) {
+			return node.parentNode === null;
+		}));
+		this.engine.nodes.length = 0;
+		return elements;
+	},
+
+	generateTags: function() {
+		this.tags.each(function(tag) {
+			this.engine[tag] = function() {
+				//console.log('Rendering tag: ', tag);
+				var el = new Element(tag);
+				$A(arguments).each(function(argument, index) {
+					//console.log('argument', index, ': ', argument, $type(argument));
+					switch ($type(argument)) {
+						case "array":
+						case "element":
+						case "collection": el.adopt(argument); break;
+						case "string": el.appendText(argument); break;
+						case "object": el.set(argument); break;
+						default: throw ("MooML: Invalid argument:" + argument.toString());
+					}
+				});
+				this.nodes.push(el);
+				return el;
+			}
+		}.bind(this));
+	}
+
+}))();
