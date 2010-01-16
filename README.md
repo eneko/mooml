2dfcd522d72a08bb8ad16eacacfda195a51c8eb3
Eneko Alonso <eneko.alonso@gmail.com>
2010-01-16 10:54:38 -0800

    Added support for html arguments. Avoid regenerating engine functions if new tags are added.

--- README.md
+++ README.md
@@ -9,53 +9,53 @@ How to use
 
 Creating Mooml templates is very easy:
 
-	Mooml.register('mytemplate', function() {
-		div({id: 'mydivid'},
-			h2('Title'),
-			p('Lorem Ipsum'),
-			p(
-				a({ href: 'http://example.com' }, 'click here')
-			)
-		);
-	});
+  Mooml.register('mytemplate', function() {
+    div({id: 'mydivid'},
+      h2('Title'),
+      p('Lorem Ipsum'),
+      p(
+        a({ href: 'http://example.com' }, 'click here')
+      )
+    );
+  });
 
 This template will generate the following HTML structure, creating the dom elements for you:
 
-	<div id="mydivid">
-		<h2>Title</h2>
-		<p>Lorem Ipsum</p>
-		<p><a href="http://example.com">click here</a></p>
-	</div>
+  <div id="mydivid">
+    <h2>Title</h2>
+    <p>Lorem Ipsum</p>
+    <p><a href="http://example.com">click here</a></p>
+  </div>
 
 To render the template and generate the dom elements:
 
-	var el = Mooml.render('mytemplate');
+  var el = Mooml.render('mytemplate');
 
 Passing variables to a template can be done easily too:
 
-	Mooml.register('mytemplate', function(params) {
-		div({id: params.myDivId}
-			// more code here
-		);
-	});
-	var el = Mooml.render('mytemplate', { myDivId: 'newid' });
+  Mooml.register('mytemplate', function(params) {
+    div({id: params.myDivId}
+      // more code here
+    );
+  });
+  var el = Mooml.render('mytemplate', { myDivId: 'newid' });
 
 Finally, we can render a template N times by passing an array as argument to the render method:
 
-	var elements = Mooml.render('mytemplate', [
-		{ myDivId: 'div1' },
-		{ myDivId: 'div2' },
-		{ myDivId: 'div3' }
-	]);
+  var elements = Mooml.render('mytemplate', [
+    { myDivId: 'div1' },
+    { myDivId: 'div2' },
+    { myDivId: 'div3' }
+  ]);
 
 Will generate:
 
-	<div id="div1">
-	</div>
-	<div id="div2">
-	</div>
-	<div id="div3">
-	</div>
+  <div id="div1">
+  </div>
+  <div id="div2">
+  </div>
+  <div id="div3">
+  </div>
 
 
 Differences between Mooml and Jaml
--- Source/mooml.js
+++ Source/mooml.js
@@ -3,12 +3,15 @@
 description: Mooml is a Mootools based version of Jaml which makes HTML generation easy and pleasurable.
 version: 1.0.5
 url: http://github.com/eneko/mooml
-Based on Ed Spencer's Jaml (http://edspencer.github.com/jaml)
+jaml: Mooml is based on Ed Spencer's Jaml (http://edspencer.github.com/jaml)
 
 license: MIT-style
 
 authors:
 - Eneko Alonso (http://enekoalonso.com)
+
+provides:
+- mooml
 
 requires:
 - core/1.2.4:Class
@@ -34,7 +37,7 @@ Mooml = new (new Class({
 	],
 
 	initialize: function() {
-		this.generateTags();
+		this.initEngine();
 	},
 
 	register: function(name, code) {
@@ -44,10 +47,7 @@ Mooml = new (new Class({
 	},
 
 	render: function(name, data) {
-		//console.log('Mooml: >>> rendering template: ', name);
-		var result = this.templates[name](data);
-		//console.log('Mooml: <<< template rendered: ', name, result);
-		return result;
+		return this.templates[name](data);
 	},
 
 	evaluate: function(code, data) {
@@ -68,26 +68,32 @@ Mooml = new (new Class({
 		return (elements.length > 1) ? elements : elements.shift();
 	},
 
-	generateTags: function() {
+	initEngine: function() {
 		this.tags.each(function(tag) {
+			if (this.engine[tag]) return;
 			this.engine[tag] = function() {
 				//console.log('Mooml: rendering tag: ', tag);
+				var nodes = this.nodeStack.getLast();
 				var el = new Element(tag);
-				$A(arguments).each(function(argument, index) {
+				$each(arguments, function(argument, index) {
 					//console.log('Mooml: argument', index, ': ', argument, '(', $type(argument), ')');
 					switch ($type(argument)) {
 						case "array":
 						case "element":
 						case "collection": el.adopt(argument); break;
-						case "string": el.appendText(argument); break;
+						case "string": {
+							el.getChildren().each(function(child) { nodes.erase(child) });
+							el.set('html', el.get('html') + argument);
+							break;
+						}
 						case "object": el.set(argument); break;
 						default: return; // ignore
 					}
 				});
-				this.nodeStack.getLast().push(el);
+				nodes.push(el);
 				return el;
 			}
-		} .bind(this));
+		}.bind(this));
 	}
 
 }))();
